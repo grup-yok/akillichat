1f2a6acfb155d1b15414fc2024e5e05cd6afe99b
Oytun <info@oytun.org>
2019-11-30 15:22:06 +0300

    initial

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -1,4 +1,4 @@
-"""akillifront URL Configuration
+"""chat URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from front import views
 
 urlpatterns = [
+    url(r'^$', views.index, name="home"),
     url(r'^admin/', admin.site.urls),
 ]
--- chat/chat/wsgi.py
+++ chat/chat/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for akillifront project.
+WSGI config for chat project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "akillifront.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
 
 application = get_wsgi_application()
