bc17936cb5d5397206562987b279b638051b976f
Oytun <info@oytun.org>
2019-12-01 00:43:17 +0300

    fixi

--- chat/front/views.py
+++ chat/front/views.py
@@ -5,13 +5,15 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
 import nltk, string
 import nltk.corpus
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize, SpaceTokenizer
+from nltk import pos_tag, ne_chunk
 #from nltk.probability import FreqDist
 from nltk.util import bigrams, trigrams, ngrams
 from fuzzywuzzy import fuzz
 from front.models import Request as req
 from front.models import Response as res
 from front.models import Dictionary as dic
+from googlesearch import search
 
 # Create your views here.
 def index(request):
@@ -24,14 +26,22 @@ def messageReceive(request):
 
     AI_tokens = word_tokenize(message)
 
+    #tok = SpaceTokenizer()
+    #toks = tok.tokenize(message)
+    #pos = pos_tag(toks)
+    #chunked_nes = ne_chunk(pos) 
+    #nes = [' '.join(map(lambda x: x[0], ne.leaves())) for ne in chunked_nes if isinstance(ne, nltk.tree.Tree)]
+
     response = ""
 
     for word in AI_tokens:
-        wrd = dic.objects.create(word=word)
-        wrd.save()
+        findDic = dic.objects.filter(word__contains=word)
+        if len(findDic) <= 0:
+            wrd = dic.objects.create(word=prounancationFixer(word))
+            wrd.save()
+        
 
         findreq = res.objects.filter(text__contains=word)
-        print(findreq)
         for x in findreq:
             rat = fuzz.token_set_ratio(x.text, word)
             if rat >= 90:
@@ -41,3 +51,26 @@ def messageReceive(request):
     return JsonResponse({
         "message": response
     })
+
+def googleSearch(query):
+    result = []
+    for i in search(query,        # The query you want to run
+                tld = 'com',  # The top level domain
+                lang = 'en',  # The language
+                num = 10,     # Number of results per page
+                start = 0,    # First result to retrieve
+                stop = None,  # Last result to retrieve
+                pause = 2.0,  # Lapse between HTTP requests
+               ):
+        result.append(i)
+    return result
+
+
+def prounancationFixer(text):
+    prounList = ['nın', 'nin', 'nun', 'nün', 'ün', 'ın', 'un', 'ni', 'nu', 'nü', 'nı']
+
+    for pron in prounList:
+        if pron in text:
+            text = text.rstrip(pron)
+
+    return text
